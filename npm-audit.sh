77fe1f2057127aa68944859e5b97e3a466738ceb
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-07 04:02:11 +0200

    jenkins

--- npm-audit.sh
+++ npm-audit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /${JENKINS_HOME}/workspace/juice-shop-pipeline
+cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
 npm i --package-lock-only
-npm audit --json > /${JENKINS_HOME}/reports/npm-audit-report
+npm audit --json > ${JENKINS_HOME}/reports/npm-audit-report
 
 #$? è una variabile che contiene il valore di ritorno dell'ultimo comando eseguito.
 echo $? > /dev/null
