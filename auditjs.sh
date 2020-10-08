ad234c5a170f4366cfffc12590b0ded85495e9ff
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-08 13:37:20 +0200

    jenkins

--- auditjs.sh
+++ auditjs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
-auditjs --username matteodalgrande97@gmail.com --token <auth_token> /var/lib/jenkins/reports/auditjs-report 2>&1
+auditjs --username matteodalgrande97@gmail.com /var/lib/jenkins/reports/auditjs-report 2>&1
 
 echo $? > /dev/null
