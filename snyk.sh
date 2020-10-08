898f451c4af02aa749263747cd8b30c234dc1510
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-08 16:37:10 +0200

    jenkins

--- snyk.sh
+++ snyk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
-snyk auth a46f83c2-3b3b-45d4-86c4-2dd897d8b594
+snyk auth 444ec264-8ca2-4384-859b-df155ed5ef6d
 snyk test --json > ${JENKINS_HOME}/reports/snyk-report
 snyk monitor
 
