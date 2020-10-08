eac343a6234938b0690182d2603a772dc9820e69
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-08 11:04:33 +0200

    jenkins

--- njsscan.sh
+++ njsscan.sh
@@ -6,6 +6,6 @@ virtualenv ${JENKINS_HOME}/venv -p python3
 source ${JENKINS_HOME}/venv/bin/activate
 pip install njsscan
 njsscan
-njsscan ${JENKINS_HOME}/workspace/juice-shop-pipeline/routes/profileImageUrlUpload.js --json -o /var/lib/jenkins/reports/njsscan-report-json
-# njsscan ${JENKINS_HOME}/workspace/juice-shop-pipeline/ --json -o /var/lib/jenkins/reports/njsscan-report-json
+# njsscan ${JENKINS_HOME}/workspace/juice-shop-pipeline/routes/profileImageUrlUpload.js --json -o /var/lib/jenkins/reports/njsscan-report-json
+njsscan ${JENKINS_HOME}/workspace/juice-shop-pipeline/ --json -o ${JENKINS_HOME}/reports/njsscan-report-json
 deactivate
