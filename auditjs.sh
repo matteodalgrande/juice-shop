94875254bb68d214638023fc0d4133598ca5bb7d
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-08 15:20:58 +0200

    jenkins

--- auditjs.sh
+++ auditjs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
-auditjs ossi ${JENKINS_HOME}/reports/auditjs-report 2>&1
+auditjs ossi --json > ${JENKINS_HOME}/reports/auditjs-report 
+# 2>&1
 
 echo $? > /dev/null
