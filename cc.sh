c61bec5b3e9b137f5cc04e1572c6c6e9a64b2dc6
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-12 19:28:41 +0200

    jenkins

--- cc.sh
+++ cc.sh
@@ -10,9 +10,9 @@ echo $GIT_BRANCH
 GIT_BRANCH=master
 echo $GIT_BRANCH
 
-./cc-test-reporter format-coverage -t lcov -o build/reports/coverage/codeclimate.frontend.json build/reports/coverage/ng/lcov.info
-./cc-test-reporter format-coverage -t lcov -o build/reports/coverage/codeclimate.server.json build/reports/coverage/server-tests/lcov.info
-./cc-test-reporter format-coverage -t lcov -o build/reports/coverage/codeclimate.api.json build/reports/coverage/api-tests/lcov.info
-./cc-test-reporter sum-coverage build/reports/coverage/codeclimate.*.json -p 3
-./cc-test-reporter upload-coverage
-./cc-test-reporter after-test
+./cc-test-reporter --debug format-coverage -t lcov -o build/reports/coverage/codeclimate.frontend.json build/reports/coverage/ng/lcov.info
+./cc-test-reporter --debug format-coverage -t lcov -o build/reports/coverage/codeclimate.server.json build/reports/coverage/server-tests/lcov.info
+./cc-test-reporter --debug format-coverage -t lcov -o build/reports/coverage/codeclimate.api.json build/reports/coverage/api-tests/lcov.info
+./cc-test-reporter --debug sum-coverage build/reports/coverage/codeclimate.*.json -p 3
+./cc-test-reporter --debug upload-coverage
+./cc-test-reporter --debug after-test
