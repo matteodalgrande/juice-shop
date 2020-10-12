2706e387a48cc817d06cf4297084683d34127eb4
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-12 19:28:01 +0200

    jenkins

---
+++ cc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+wget https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64  > ./cc-test-reporter
+chmod 777  cc-test-reporter
+
+echo $GIT_COMMIT
+GIT_COMMIT=$(git log | grep -m1 -oE \'[^ ]+$\')
+echo $GIT_COMMIT
+
+echo $GIT_BRANCH
+GIT_BRANCH=master
+echo $GIT_BRANCH
+
+./cc-test-reporter format-coverage -t lcov -o build/reports/coverage/codeclimate.frontend.json build/reports/coverage/ng/lcov.info
+./cc-test-reporter format-coverage -t lcov -o build/reports/coverage/codeclimate.server.json build/reports/coverage/server-tests/lcov.info
+./cc-test-reporter format-coverage -t lcov -o build/reports/coverage/codeclimate.api.json build/reports/coverage/api-tests/lcov.info
+./cc-test-reporter sum-coverage build/reports/coverage/codeclimate.*.json -p 3
+./cc-test-reporter upload-coverage
+./cc-test-reporter after-test
