fb0f5acb356430d4b9a87d88d86164e8f45e024e
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-20 18:57:43 +0200

    jenkins

--- jshint-script.sh
+++ jshint-script.sh
@@ -3,6 +3,6 @@ cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
 pwd
 jshint $(ls ./**/*.js | grep -v node | grep js) *.js > ${JENKINS_HOME}/reports/jshint-report-server
 cd ${JENKINS_HOME}/workspace/juice-shop-pipeline/frontend
-jshint $(ls ./**/**/*.ts | grep -v node | grep js) *.js > ${JENKINS_HOME}/reports/jshint-report-frontend
+jshint $(ls ./**/*.ts | grep -v node | grep ts) *.ts > ${JENKINS_HOME}/reports/jshint-report-frontend
 
 echo $? > /dev/null
