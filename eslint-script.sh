ab9ae17110e3e745d85c55736e48c0e65afd0fed
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-20 22:04:18 +0200

    jenkins

--- eslint-script.sh
+++ eslint-script.sh
@@ -5,6 +5,6 @@ pwd
 eslint --no-color --format json --ext .js --output-file ${JENKINS_HOME}/reports/eslint-report-server ./
 cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
 pwd
-eslint --no-color --format json --ext .js .ts --output-file ${JENKINS_HOME}/reports/eslint-report-server ./
+eslint --no-color --format json --ext .js .ts --output-file ${JENKINS_HOME}/reports/eslint-report-frontend ./
 
 echo $? > /dev/null
