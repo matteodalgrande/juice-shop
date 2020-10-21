36c1dc502d5e4a572743f60f117a25231966eff1
matteodalgrande <matteodalgrande97@gmail.com>
2020-10-21 15:25:56 +0200

    jenkins

--- eslint-script.sh
+++ eslint-script.sh
@@ -3,8 +3,15 @@
 cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
 pwd
 eslint --no-color --format json --ext .js --output-file ${JENKINS_HOME}/reports/eslint-report-server ./
-cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
+cd ${JENKINS_HOME}/workspace/juice-shop-pipeline/frontend
 pwd
 eslint --no-color --format json --ext .ts --output-file ${JENKINS_HOME}/reports/eslint-report-frontend ./
+
+cd ${JENKINS_HOME}/reports/
+pwd
+wget https://raw.githubusercontent.com/matteodalgrande/prettyPrint-json-file-python/master/prettyPrint-json-file-python.py
+python3 prettyPrint-json-file-python.py ${JENKINS_HOME}/reports/eslint-report-frontend
+python3 prettyPrint-json-file-python.py ${JENKINS_HOME}/reports/eslint-report-frontend
+rm prettyPrint-json-file-python.py
 
 echo $? > /dev/null
