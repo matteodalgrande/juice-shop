2cd512412e34796a6abb3b1ec53966e780134de5
matteodalgrande <matteodalgrande97@gmail.com>
2020-11-11 03:01:17 +0100

    jenkins

--- jshint-script.sh
+++ jshint-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ${JENKINS_HOME}/workspace/juice-shop-pipeline
 pwd
-jshint $(ls ./**/*.js | grep -v node | grep *js$) *.js > ${JENKINS_HOME}/reports/jshint-report-server
+##jshint $(ls ./**/*.js | grep -v node | grep *js$) *.js > ${JENKINS_HOME}/reports/jshint-report-server
+jshint $(ls ./**/*.js | grep [.]js$) *.js > ${JENKINS_HOME}/reports/jshint-report-server
+
 #jshint non supporta file typescript
 echo $? > /dev/null
